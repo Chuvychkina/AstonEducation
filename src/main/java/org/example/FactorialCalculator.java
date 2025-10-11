57f342f6e4d27be2d2e8c98172380302ee3f6f63
Chuvychkina Tatiana <chuvichkina.t@yandex.ru>
2025-10-11 22:22:07 +0300

    Lesson_7_junit5

--- src/main/java/org/example/FactorialCalculator.java
+++ src/main/java/org/example/FactorialCalculator.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class FactorialCalculator {
-    public static double triangleArea(double base, double height) {
-        return (base * height) / 2;
+    /**
+     * Вычисляет факториал числа n (n!).
+     * @param n неотрицательное целое число
+     * @return факториал n
+     * @throws IllegalArgumentException если n < 0
+     */
+    public static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Факториал не определён для отрицательных чисел");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
--- src/main/java/org/example/TriangleAreaCalculator.java
+++ src/main/java/org/example/TriangleAreaCalculator.java
@@ -1,13 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class TriangleAreaCalculator {
-    public static String compareNumbers(int a, int b) {
-        if (a > b) {
-            return a + " больше " + b;
-        } else if (a < b) {
-            return a + " меньше " + b;
-        } else {
-            return a + " равно " + b;
+    /**
+     * Вычисляет площадь треугольника по основанию и высоте.
+     * @param base длина основания (должна быть > 0)
+     * @param height высота (должна быть > 0)
+     * @return площадь треугольника
+     * @throws IllegalArgumentException если base <= 0 или height <= 0
+     */
+    public static double area(double base, double height) {
+        if (base <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Основание и высота должны быть положительными");
         }
+        return (base * height) / 2.0;
     }
 }
+
